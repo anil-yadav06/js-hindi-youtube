e5ccb6fdee4d5e06c740f9e4ad7db3ecbe334e89
ANIL YADAV <anilyadav902658@gmail.com>
2025-06-26 07:48:07 +0000

    conversions of datatypes

---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,33 @@
+//let score= "33abc"
+//console.log(typeof(score))
+//let valueInnumber = Number(score)//use to find number in string
+//console.log(typeof valueInnumber)
+//console.log(valueInnumber)
+
+
+
+//"33"=>33
+//"33abc"=> NaN
+//true=> true
+
+
+
+
+//let islogedin = "anil"
+//let BooleanIsLoggedIn=Boolean(islogedin)
+//console.log(typeof islogedin)
+//console.log(BooleanIsLoggedIn)
+
+
+
+//1=> true ; o=> false
+//"" => false
+//"anil" => true
+
+
+
+
+let somenumber=33
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log( typeof stringnumber);
