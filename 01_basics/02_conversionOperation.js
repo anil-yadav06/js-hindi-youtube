790562c9351dc16b303df2ccef475859b48d87fa
ANIL YADAV <anilyadav902658@gmail.com>
2025-06-27 09:59:37 +0000

    Pending changes exported from your codespace

--- 01_basics/02_conversionOperation.js
+++ 01_basics/02_conversionOperation.js
@@ -27,7 +27,40 @@
 
 
 
-let somenumber=33
-let stringnumber = String(somenumber)
-console.log(stringnumber);
-console.log( typeof stringnumber);
+//let somenumber=33
+//let stringnumber = String(somenumber)
+//console.log(stringnumber);
+//console.log( typeof stringnumber);
+
+
+
+
+
+
+//***********************Operations****************
+
+
+//let value =3
+//let negValue =-value
+//console.log(negValue)
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2**2);
+
+
+let str1 = "hello "
+let str2 = "anil"
+let str3 = str1 + str2
+console.log(str3);
+
+
+
+console.log("1"+ 2);
+console.log(1+"2");
+console.log("1"+ 2+2);
+console.log(1+2+"2");
+
+
+
+
